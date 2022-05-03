95bca2642cf70f781093f6c8fa7c41fdbf6385c0
Jhoemar <jhoemar_pagao@yahoo.com>
2022-05-03 22:12:14 +0800

    remove walletconnect crypyo

--- packages/clients/client/src/index.ts
+++ packages/clients/client/src/index.ts
@@ -1,6 +1,6 @@
 import Connector from "@walletconnect/core";
 import { IWalletConnectOptions, IPushServerOptions } from "@walletconnect/types";
-import * as cryptoLib from "@walletconnect/iso-crypto";
+import * as cryptoLib from "@elabox/iso-crypto";
 
 class WalletConnect extends Connector {
   constructor(connectorOpts: IWalletConnectOptions, pushServerOpts?: IPushServerOptions) {
